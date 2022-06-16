faeefa87dc85108b74cdb9656e2a1597163a626b
srlegramante <101984333+srlegramante@users.noreply.github.com>
2022-06-16 20:38:28 -0300

    first commit

---
+++ src/app/cadastro/cadastro.page.ts
@@ -0,0 +1,71 @@
+import { StorageService } from './../services/storage.service';
+import { comparaValidator } from './../validators/compara-validator';
+import { CpfValidator } from './../validators/cpf-validator';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Usuario } from '../models/usuario';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.page.html',
+  styleUrls: ['./cadastro.page.scss'],
+})
+export class CadastroPage implements OnInit {
+
+  formCadastro: FormGroup;
+  usuario: Usuario = new Usuario();
+
+  mensagens = {
+    nome: [
+      {tipo: 'required', mensagem: 'O campo Nome é obrigatório!'},
+      {tipo: 'minlength', mensagem: 'O campo nome precisa ter pelo menos 3 caracteres!'}
+    ],
+    cpf: [
+      {tipo: 'required', mensagem: 'O campo CPF é obrigatório!'},
+      {tipo: 'invalido', mensagem: 'CPF invalido'}
+    ],
+    email: [
+      { tipo: 'required', mensagem: 'O campo E-mail é obrigatório.' },
+      { tipo: 'email', mensagem: 'E-mail Inválido.' },
+    ],
+    senha: [
+      { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+      { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+    ],
+    confirmaSenha: [
+      { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+      { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+      { tipo: 'comparacao', mensagem: 'Deve ser igual a senha.' },
+    ],
+  };
+
+  constructor(private formBuilder: FormBuilder, private storageService: StorageService, private route: Router) {
+    this.formCadastro = this.formBuilder.group({
+      nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      cpf: ['', Validators.compose([Validators.required, CpfValidator.cpfValido])],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      senha: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(25)])],
+      confirmaSenha: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(25)])]
+    },{
+      validator: comparaValidator('senha', 'confirmaSenha')
+    });
+  }
+
+  ngOnInit() {
+  }
+  async salvarCadastro(){
+    if(this.formCadastro.valid){
+      this.usuario.nome = this.formCadastro.value.nome;
+      this.usuario.cpf = this.formCadastro.value.cpf;
+      this.usuario.email = this.formCadastro.value.email;
+      this.usuario.senha = this.formCadastro.value.senha;
+      await this.storageService.set(this.usuario.email, this.usuario);
+      this.route.navigateByUrl('/home');
+    }else{
+      alert('Formulário invalido');
+    }
+  }
+}
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,27 @@
+import { StorageService } from './../services/storage.service';
+import { Usuario } from './../models/usuario';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+    listaUsuarios: Usuario[] = [];
+
+  constructor(private storageService: StorageService) {}
+
+  async buscarUsuarios(){
+    this.listaUsuarios = await this.storageService.getAll();
+  }
+
+  ionViewDidEnter(){
+    this.buscarUsuarios();
+  }
+  async excluirCadastro(email: string){
+    await this.storageService.remove(email);
+    this.buscarUsuarios();
+  }
+}
